068d854978b50bc1032fdaa95ba7d11943b3beba
YafeiMaoLab <106758259+YafeiMaoLab@users.noreply.github.com>
2023-04-20 18:17:41 +0800

    Delete pav.sh

--- pav.sh
+++
@@ -1,4 +0,0 @@
-pav
-the pipeline is from "https://github.com/EichlerLab/pav"
-#align parameters used in this study
-minimap2 -x asm20 -m 10000 -z 10000,50 -r 50000 --end-bonus=100 --secondary=no -a -t {params.cpu} --eqx -Y -O 5,56 -E 4,1 -B 5 {input.ref_fa} {input.fa}
